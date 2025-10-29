871b3ea4a516a10a61ec59c588f3556995d7a99a
Syed Abid Ali Abdi <syedabidaliabdi@gmail.com>
2025-10-29 16:44:43 +0500

    aspne core app added

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Core.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
